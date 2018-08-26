f91825db2ec7253f29334d44a5e262b49023177b
Wei Lu <luwei.here@gmail.com>
2018-08-27 00:10:28 +0800

    Naive Bayes model

--- BT5152_Tutorial_1/KNN_Naive_Bayes_and_Decision_Tree/initLesson.R
+++ BT5152_Tutorial_1/KNN_Naive_Bayes_and_Decision_Tree/initLesson.R
@@ -7,3 +7,7 @@ iris_data$id <- 1:nrow(iris_data)
 
 iris_train <- iris_data %>% sample_frac(2/3)
 iris_test <- anti_join(iris_data, iris_train, by = 'id')
+
+# remove id column from both datasets
+iris_train <- iris_train %>% select(-id)
+iris_test <- iris_test %>% select(-id)
