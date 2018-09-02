4383700da86dad9a30f3fd6b25975f9c0c0e9efc
Wei Lu <luwei.here@gmail.com>
2018-09-03 01:19:56 +0800

    Rename variable to avoid conflict

--- BT5152_Tutorial_2/Performance_Metrics/initLesson.R
+++ BT5152_Tutorial_2/Performance_Metrics/initLesson.R
@@ -6,17 +6,17 @@ library(e1071)
 data("BreastCancer")
 
 breast_cancer <- as.tibble(BreastCancer)
-train <- withr::with_seed(42, sample_frac(breast_cancer, 0.8))
+training <- withr::with_seed(42, sample_frac(breast_cancer, 0.8))
 test <- breast_cancer %>%
-  anti_join(train, by = 'Id') %>% # side effect of anti_join: rows with NA removed
+  anti_join(training, by = 'Id') %>% # side effect of anti_join: rows with NA removed
   select(-Id)
-train <- train %>% select(-Id)
+training <- training %>% select(-Id)
 
 # prepare the decision classifier
-model_c50 <- C5.0(Class ~ ., data=train)
+model_c50 <- C5.0(Class ~ ., data=training)
 
 # build a naive bayes classifer & prepare the roc plottable object for comparison purpose
-model_nb <- naiveBayes(Class ~ ., data=train)
+model_nb <- naiveBayes(Class ~ ., data=training)
 predictions_prob_nb <- predict(model_nb, test, type='raw')[, 2]
 pred_nb <- prediction(predictions_prob_nb, labels=test$Class)
 roc_nb <- performance(pred_nb, measure='tpr', x.measure='fpr')
