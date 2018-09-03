66d0d54472cd6f5ea0810ac8b5c93cc7a3896052
Wei Lu <luwei.here@gmail.com>
2018-09-03 13:37:22 +0800

    Fix data preparation
    Don’t use non-unique Id & remove NA

--- BT5152_Tutorial_2/ROC_and_AUC/initLesson.R
+++ BT5152_Tutorial_2/ROC_and_AUC/initLesson.R
@@ -5,12 +5,18 @@ library(e1071)
 
 data("BreastCancer")
 
-breast_cancer <- as.tibble(BreastCancer)
+# remove any row with NA values
+breast_cancer <- as.tibble(BreastCancer) %>%
+  filter(complete.cases(.))
+
+# Ids are not unique, so we create an index column
+breast_cancer$index <- 1:nrow(breast_cancer)
+
 training <- withr::with_seed(42, sample_frac(breast_cancer, 0.8))
 test <- breast_cancer %>%
-  anti_join(training, by = 'Id') %>% # side effect of anti_join: rows with NA removed
+  anti_join(training, by = 'index') %>%
   select(-Id)
-training <- training %>% select(-Id)
+training <- training %>% select(-index)
 
 # prepare the decision classifier
 model_c50 <- C5.0(Class ~ ., data=training)
