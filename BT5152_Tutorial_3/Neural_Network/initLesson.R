e267385615c19242e3f9ea6bd6748e9a1e1b11ce
Wei Lu <luwei.here@gmail.com>
2018-09-10 14:09:00 +0800

    Switch dataset package & scale data to range for faster training

--- BT5152_Tutorial_3/Neural_Network/initLesson.R
+++ BT5152_Tutorial_3/Neural_Network/initLesson.R
@@ -1,65 +1,72 @@
 library(tidyverse)
-library(MASS)
+library(mlbench)
 library(Metrics)
 library(neuralnet)
 
-data('Boston')
+options(dplyr.width = Inf)
 
-set.seed(42)
+data('BostonHousing')
 
-boston <- as.tibble(Boston)
-boston$index <- 1:nrow(Boston)
+set.seed(1337)
+
+normalize <- function(x) {
+  return((x - min(x)) / (max(x) - min(x)))
+}
+
+denormalize <- function(x, minx, maxx) {
+  return(x * (maxx - minx) + minx)
+}
+
+boston <- as.tibble(BostonHousing) %>%
+  mutate(chas = as.numeric(levels(chas)[chas]))
+boston$index <- 1:nrow(boston)
 
 training <- withr::with_seed(42, sample_frac(boston, 0.8))
 test <- boston %>%
   anti_join(training, by = 'index') %>%
-  dplyr::select(-index)
-training <- training %>% dplyr::select(-index)
+  select(-index)
+training <- training %>% select(-index)
 
 # standardize training & test data
-scaled_training <- training %>% mutate_all(scale)
-scaled_test <- test %>% mutate_all(scale)
+scaled_training <- training %>% mutate_all(normalize)
+scaled_test <- test %>% mutate_all(normalize)
 
 # create the formula field
 col_names <- names(scaled_training)
 formula <- as.formula(paste("medv ~", paste(col_names[!col_names %in% "medv"], collapse = " + ")))
 
-# default parameters doesn't converge in time
 model_nn <- neuralnet(formula, data=scaled_training)
-
-# increase stepmax to give it more time to converge
-model_nn <- neuralnet(formula, data=scaled_training, stepmax=1e6)
-plot(model_nn)
+# plot(model_nn)
 
 # The compute() function works a bit differently from the predict() functions we've used so far.
 # It returns a list with two components: $neurons, which stores the neurons for each layer in the network, and $net.result, which stores the predicted values.
-results <- compute(model_nn, dplyr::select(scaled_test, -medv))
+results <- compute(model_nn, select(scaled_test, -medv))
 scaled_pred <- results$net.result
 
 mae(scaled_test$medv, scaled_pred)
 
-# 1 layer with 5 nodes
-model_nn_1x5 <- neuralnet(formula, data=scaled_training, stepmax=1e6, hidden=5)
-plot(model_nn_1x5)
-results_1x5 <- compute(model_nn_1x5, dplyr::select(scaled_test, -medv))
-scaled_pred_1x5 <- results_1x5$net.result
-mae(scaled_test$medv, scaled_pred_1x5)
+# 1 layer with 2 nodes
+model_nn_2x1 <- neuralnet(formula, data=scaled_training, hidden=3)
+# plot(model_nn_2x1)
+results_2x1 <- compute(model_nn_2x1, select(scaled_test, -medv))
+scaled_pred_2x1 <- results_2x1$net.result
+mae(scaled_test$medv, scaled_pred_2x1)
 
-# 2 layers with total 5 nodes
-model_nn_2x3 <- neuralnet(formula, data=scaled_training, stepmax=1e6, hidden=c(3, 2))
-plot(model_nn_2x3)
-results_2x3 <- compute(model_nn_2x3, dplyr::select(scaled_test, -medv))
-scaled_pred_2x3 <- results_2x3$net.result
-mae(scaled_test$medv, scaled_pred_2x3)
+# 2 layers with 3 nodes and 2 nodes each
+model_nn_3x2 <- neuralnet(formula, data=scaled_training, hidden=c(3, 2))
+# plot(model_nn_3x2)
+results_3x2 <- compute(model_nn_3x2, select(scaled_test, -medv))
+scaled_pred_3x2 <- results_3x2$net.result
+mae(scaled_test$medv, scaled_pred_3x2)
 
 # use a different activation function
-model_nn_tanh <- neuralnet(formula, data=scaled_training, stepmax=1e6, act.fct='tanh')
-results_tanh <- compute(model_nn_tanh, dplyr::select(scaled_test, -medv))
+model_nn_tanh <- neuralnet(formula, data=scaled_training, hidden=c(3, 2), act.fct='tanh')
+results_tanh <- compute(model_nn_tanh, select(scaled_test, -medv))
 scaled_pred_tanh <- results_tanh$net.result
 mae(scaled_test$medv, scaled_pred_tanh)
 
 # unscale predicted values
-pred <- scaled_pred * attr(scaled_test$medv, 'scaled:scale') + attr(scaled_test$medv, 'scaled:center')
+pred <- denormalize(scaled_pred, min(scaled_test$medv), max(scaled_test$medv))
 
 # visualize actual values vs. predicted values
 plot(test$medv, pred)
