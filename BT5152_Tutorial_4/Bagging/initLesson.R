9dc1976213a2d178fbeecb63adbb17b624d69238
Wei Lu <luwei.here@gmail.com>
2018-09-17 00:15:41 +0800

    Switch bagging script to regression dataset
    leave classification to boosting,
    as adaboost only works with classification

--- BT5152_Tutorial_4/Bagging/initLesson.R
+++ BT5152_Tutorial_4/Bagging/initLesson.R
@@ -1,81 +1,83 @@
 library(tidyverse)
-library(mlbench)
 library(rpart)
 library(caret)
 library(randomForest)
+library(Metrics)
+library(ggplot2)
 
 options(dplyr.width = Inf)
 
-data('Ionosphere')
+data('diamonds')
 
 set.seed(1337)
 
-Ionosphere <- Ionosphere[,-2]
-Ionosphere$V1 <- as.numeric(as.character(Ionosphere$V1))
-Ionosphere$index <- 1:nrow(Ionosphere)
+sample_indexes <- withr::with_seed(42, sample(1:nrow(diamonds), 1000))
+diamonds_sample <- diamonds[sample_indexes, ]
+data <- as.tibble(diamonds_sample)
+data$index <- 1:nrow(diamonds_sample)
 
-ionosphere <- as.tibble(Ionosphere)
-
-training <- withr::with_seed(42, sample_frac(Ionosphere, 0.8))
-test <-ionosphere %>%
+training <- withr::with_seed(42, sample_frac(data, 0.8))
+test <-data %>%
   anti_join(training, by = 'index') %>%
   dplyr::select(-index)
 training <- training %>% dplyr::select(-index)
 
-model <- rpart(Class ~., training)
+model <- rpart(price ~., training)
+out_train <- predict(model, training)
+rpart_mae_train <- mae(training$price, out_train)
+out <- predict(model, test)
+rpart_mae <- mae(test$price, out)
+print(c(rpart_mae_train, rpart_mae,
+        rpart_mae - rpart_mae_train))
 
-out_train <- predict(model, training, type='class')
-accuracy_train <- mean(training$Class == out_train)
-out <- predict(model, test, type='class')
-accuracy <- mean(test$Class == out)
-print(c(accuracy_train, accuracy,
-        accuracy_train - accuracy))
 
 # build manual bagging models
 train_bagging <- function(n_bags, training_data){
   models <- lapply(1:n_bags, function(i) {
     n_sample <- nrow(training_data)
     sample_indexes <- sample(1:n_sample, replace = TRUE)
-    return(rpart(Class ~., training_data[sample_indexes, ]))
+    return(rpart(price ~., training_data[sample_indexes, ]))
   })
   return(models)
 }
 
 # use bagging models to predict
 predict_bagging <- function(models, test_data) {
-  probs <- sapply(models, function(m) {
-    return(as.data.frame(predict(m, test_data, type='prob'))[, -1]) # get the predicted prob of the positive class
+  prices <- sapply(models, function(m) {
+    return(predict(m, test_data))
   })
-  avg_probs <- rowSums(probs) / length(models)
-  return(factor(avg_probs>0.5, levels=c(T, F), labels=c('good', 'bad')))
+  return(rowSums(prices) / length(models))
 }
 
-models <- train_bagging(50, training)
-out_bagging <- predict_bagging(models, test)
-accuracy_bagging <- mean(test$Class == out_bagging)
+models <- train_bagging(25, training)
 out_bagging_train <- predict_bagging(models, training)
-accuracy_bagging_training <- mean(training$Class == out_bagging_train)
-print(c(accuracy_bagging_training, accuracy_bagging,
-        accuracy_bagging_training - accuracy_bagging))
+bagging_mae_train <- mae(training$price, out_bagging_train)
+out_bagging <- predict_bagging(models, test)
+bagging_mae <- mae(test$price, out_bagging)
+print(c(bagging_mae_train, bagging_mae,
+        bagging_mae - bagging_mae_train))
 
 
 # use 5-fold cv for model selection
 control <- trainControl(method="cv", number=5)
+metric <- 'MAE'
 
 # bagged CART using caret
-model_treebag <- train(Class~., data=training, method="treebag", trControl=control, control=rpart.control())
-out_train_treebag <- predict(model_treebag, training, type='raw')
-accuracy_train_treebag <- mean(training$Class == out_train_treebag)
-out_treebag <- predict(model_treebag, test, type='raw')
-accuracy_treebag <- mean(test$Class == out_treebag)
-print(c(accuracy_train_treebag, accuracy_treebag,
-        accuracy_train_treebag - accuracy_treebag))
+model_treebag <- train(price~., data=training, method="treebag", control=rpart.control(),
+                       metric=metric, trControl=control)
+out_train_treebag <- predict(model_treebag, training)
+treebag_mae_train <- mae(training$price, out_train_treebag)
+out_treebag <- predict(model_treebag, test)
+treebag_mae <- mae(test$price, out_treebag)
+print(c(treebag_mae_train, treebag_mae,
+        treebag_mae - treebag_mae_train))
 
 # bagging with random forest
-model_rf <- train(Class~., data=training, method="rf", trControl=control)
-out_train_rf <- predict(model_rf, training, type='raw')
-accuracy_train_rf <- mean(training$Class == out_train_rf)
-out_rf <- predict(model_rf, test, type='raw')
-accuracy_rf <- mean(test$Class == out_rf)
-print(c(accuracy_train_rf, accuracy_rf,
-        accuracy_train_rf - accuracy_rf))
+model_rf <- train(price~., data=training, method="rf",
+                  metric=metric, trControl=control)
+out_train_rf <- predict(model_rf, training)
+rf_mae_train <- mae(training$price, out_train_rf)
+out_rf <- predict(model_rf, test)
+rf_mae <- mae(test$price, out_rf)
+print(c(rf_mae_train, rf_mae,
+        rf_mae - rf_mae_train))
